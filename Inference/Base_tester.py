785bc175c2a95d19507779c5f3dd054652c62616
Aditya Mohanty <109948376+AdityaMohanty374@users.noreply.github.com>
2025-10-05 00:39:43 +0530

    Update and rename Base_tester.py to Inference/Base_tester.py

---
+++ Inference/Base_tester.py
@@ -0,0 +1,19 @@
+# Simple usage
+from inference_system import SignLanguagePredictor
+
+# Initialize with your .pth file
+predictor = SignLanguagePredictor(f"Model path to {BaseModel.pth}")
+
+# Predict single image
+result = predictor.predict_image("Single Image Dir")
+print(f"Predicted: {result['top_prediction']['letter']}")
+
+# Visualize results
+predictor.visualize_prediction("Single Image Dir")
+
+# Batch process
+#predictor.batch_predict('Image Dataset Path', output_csv=f'Save Location for: {base_results.csv}')
+
+# Real-time webcam
+
+#predictor.predict_webcam()
